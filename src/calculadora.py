f9217b4c3c39dd41d002d80c86eaa57de000dc3e
RAMIRO <ramiroecg2@gmail.com>
2026-06-15 21:44:23 -0300

    corregi espacios calculadora.py

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,16 +1,20 @@
 def sumar(a, b):
     return a + b
 
+
 def restar(a, b):
     return a - b
 
+
 def multiplicar(a, b):
     return a * b
+
 
 def dividir(a, b):
     if b == 0:
         raise ValueError("No se puede dividir por cero")
     return a / b
+
 
 def potencia(base, exponente):
     return base ** exponente
