a97fcd7bfe6337a468535245c83d30dd5403195a
pochin12 <rambocant12@gmail.com>
2026-06-18 15:40:09 -0300

    error en prueba, suma, corregido

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -6,7 +6,7 @@ from calculadora import sumar, restar, multiplicar, dividir, potencia
 import pytest
 
 def test_sumar():
-    assert sumar(2, 3) == 8
+    assert sumar(2, 3) == 5
 
 def test_restar():
     assert restar(10, 4) == 6
